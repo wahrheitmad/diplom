8da485adbdacb240c3a6e3ddd8c290cc138f3479
Liana G <lostrich13@gmail.com>
2024-03-30 20:57:43 +0300

    Построение изначальной поверхности (зафиналила точно!, вроде все кажется логичным)

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
-import random
 import matplotlib.pyplot as plt
 from sympy import symbols, sympify
+from numpy import sin, cos, pi
 
 # Класс сетки
 class Grid:
@@ -22,19 +22,11 @@ class Grid:
     def len(self):
         return abs(self.x_min) + abs(self.x_max)
 
-
-    # Инициализация сетки
-    def grid_init(self):
-        x = np.linspace(self.x_min, self.x_max, self.len())
-        y = np.linspace(self.y_min, self.y_max, self.len())
-        return x, y
-
-
 # Класс уравнения
 class Equation(Grid):
-    def __init__(self, x_min, x_max, y_min, y_max, length):
+    def __init__(self, x_min, x_max, y_min, y_max, length, equation):
         super().__init__(x_min, x_max, y_min, y_max, length)
-
+        self.equation = equation
 
     # Массив значений X
     def x_values(self):
@@ -53,32 +45,30 @@ class Equation(Grid):
         return np.array(y)
 
     # Преобразование строки уравнения для вычислений
-    def calculate_equation(self, equation, x_value, y_value, pi_value=np.pi):
-        x, y, pi = symbols('x y pi')
-        expr = sympify(equation)  # Преобразование строки уравнения в символьное выражение
-        result = expr.subs({x: x_value, y: y_value, pi: pi_value})  # Подстановка значений переменных в уравнение
-        result = result.evalf() # Оценка символьного выражения для получения числового значения
-        return result
+    # def calculate_equation(self, equation, x_value, y_value, pi_value=np.pi):
+    #     x, y, pi = symbols('x y pi')
+    #     expr = sympify(equation)  # Преобразование строки уравнения в символьное выражение
+    #     result = expr.subs({x: x_value, y: y_value, pi: pi_value})  # Подстановка значений переменных в уравнение
+    #     result = result.evalf() # Оценка символьного выражения для получения числового значения
+    #     return result
 
     # Массив значений Z
-    def z_values(self, equation):
-        x = self.x_values()
-        y = self.y_values()
+    def z_values(self, x, y):
         z = []
         for i in range(0, self.len()**2):
-            temp = self.calculate_equation(equation, x[i], y[i])
-            z.append(temp)
-        z = np.array(y)
-        return x, y, z
+            z.append(eval(self.equation, {"sin": sin, "cos": cos, "pi": pi, "x": x[i], "y": y[i]}))
+        z = np.array(z)
+        return z
 
     # Визуализация поверхности
-    def visual(self, equation):
-        x, y, z = self.z_values(equation);
+    def visual(self, x, y):
         xgrid, ygrid = np.meshgrid(x, y)
-        zgrid = 1 - (xgrid/8) ** 2 + 0.5 * np.sin(np.pi/4 * ygrid)
+        # zgrid = 1 - (xgrid/8) ** 2 + 0.5 * np.sin(np.pi/4 * ygrid)
+        # zgrid = self.calculate_equation("1 - (x/8) ** 2 + 0.5 * sin(pi/4 * y)", xgrid, ygrid)
+        zgrid = eval(self.equation, {"sin": sin, "cos": cos, "pi": pi, "x": xgrid, "y": ygrid})
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        surf = ax.plot_surface(xgrid, ygrid, zgrid, rstride = 2, cstride = 2, cmap='viridis')
+        surf = ax.plot_surface(xgrid, ygrid, zgrid, rstride = 1, cstride = 1, cmap='viridis')
 
         # Добавляем метки и цветовую шкалу
         ax.set_xlabel('X')
@@ -92,12 +82,14 @@ class Equation(Grid):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    g = Equation(-8, 8, -8, 8, 1)
+    g = Equation(-8, 8, -8, 8, 1, "1 - (x/8) ** 2 + 0.5 * sin(pi/4 * y)")
     # print(g.x_values())
     # print('\n\n\n')
     # print(g.y_values())
     # print('\n\n\n')
     # print(g.z_values("1 - (x/8) ** 2 + 0.5 * sin(pi/4 * y)"))
-    g.visual("1 - (x/8) ** 2 + 0.5 * sin(pi/4 * y)")
+    x = g.x_values()
+    y = g.y_values()
+    g.visual(x, y)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
